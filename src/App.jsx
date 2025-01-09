391207f4c339ba47d02970ba5b1a0da25ed400d2
Jurre van Eijk <jurreveijk13@gmail.com>
2025-01-09 12:30:19 +0800

    State [Pokemom state]: pokemon that get fetched now also get their own state so that I can map over them

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+	const [pokemon, setPokemon] = useState([]);
+
+	// Function to fetch data
 	function getData() {
 		fetch("https://pokeapi.co/api/v2/pokemon/?limit=11&offset=900")
 			.then((response) => {
@@ -12,16 +15,23 @@ function App() {
 				return response.json();
 			})
 			.then((data) => {
-				console.log("Data received:", data);
+				console.log("Data received:", data.results);
+				setPokemon(data.results);
 			})
 			.catch((error) => {
 				console.error("There was a problem with the fetch operation:", error);
 			});
 	}
 
+	// Fetch data on component mount
 	useEffect(() => {
 		getData();
 	}, []);
+
+	// Log updated pokemon state
+	useEffect(() => {
+		console.log("Pokemon state updated:", pokemon);
+	}, [pokemon]);
 
 	return (
 		<div className="maincontent">
@@ -30,7 +40,7 @@ function App() {
 				<h1>Pokemon memory game</h1>
 				<h1 className="highScore">High score:</h1>
 			</div>
-			<div className="cardsDiv">Je dikke kanker moeder</div>
+			<div className="cardsDiv"></div>
 		</div>
 	);
 }
